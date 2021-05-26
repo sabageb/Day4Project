b216f23e6542cf3541c619726ad5963bbe1a0430
Saba <sabageb@gmail.com>
2021-05-26 02:59:00 -0700

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -3,5 +3,10 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        //created new project
+        //enabled version control
+        //added .gitignore with .idea, target folder and *.iml file
+        //made initial commit by keeping track of all files
+        // shared project on github
     }
 }
